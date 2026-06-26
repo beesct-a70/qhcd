f530926e7719cf68889e5d6476620825f1ea17b9
ntkien-0110 <beesct.a70@gmail.com>
2026-06-26 10:33:20 +0700

    commit api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,6 @@
 // CẤU HÌNH GOOGLE APPS SCRIPT URL
 const GOOGLE_APPS_SCRIPT_URL = process.env.NEXT_PUBLIC_GOOGLE_APPS_SCRIPT_URL || "";
 const SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY || "";
-<<<<<<< HEAD
 
 // CẤU HÌNH THÔNG TIN NGÂN HÀNG
 export const BANK_INFO = {
@@ -10,8 +9,6 @@ export const BANK_INFO = {
   accountName: process.env.NEXT_PUBLIC_BANK_ACCOUNT_NAME || "",
   bankName: process.env.NEXT_PUBLIC_BANK_NAME || ""
 };
-=======
->>>>>>> 055a9355dd8ef3e2efa5ad6587a978874db4ef3f
 
 // CÁC ENDPOINT CHỨC NĂNG
 export const API_ENDPOINTS = {
